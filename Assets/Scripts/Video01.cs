fb73152fb478bc02fefb4f0d6cff01e239783aef
Fran-Godoy <101577372+Fran-Godoy@users.noreply.github.com>
2022-04-19 10:37:20 -0300

    Excercise 01 completion

---
+++ Assets/Scripts/Video01.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video01 : MonoBehaviour
+{
+    public int edadusuario; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edadusuario >= 18)
+        {
+            Debug.Log ("Es mayor de edad"); 
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
