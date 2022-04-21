794ee5ba98e3ceee3726ec2112dc4d037667a37a
Fran-Godoy <101577372+Fran-Godoy@users.noreply.github.com>
2022-04-21 15:54:14 -0300

    Advances password checker

---
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    Text ingresoUsuario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void ValidarContraseña()
+        {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+}
+//Quedaste en el minuto 5:24
